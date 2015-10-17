cf9a67a1f45cf1ccd9293db09fe0506ac884e031
Connor Henke <henkec@msoe.edu>
2015-10-17 10:25:09 -0500

    some of these things look like they should be ignored

---
+++ src/main/java/everything/MinecraftHackathon.java
@@ -0,0 +1,17 @@
+package everything;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MinecraftHackathon extends JavaPlugin {
+	
+	@Override
+	public void onEnable() {
+		
+	}
+
+	@Override
+	public void onDisable() {
+		
+	}
+
+}
